82ee82575dad193fdfe362e0ec07655ac67e6173
sfmalloy <sfmalloy@millersville.edu>
2020-09-15 18:10:16 -0400

    Started main.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,152 @@
 /**********************************************************************/
 // System includes
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <string>
 
 /**********************************************************************/
 // Local includes
 #include "matrix.hpp"
+
+/**********************************************************************/
+// Using declarations
+using matrix_t = float;
+using tokenlist_t = std::vector<std::string>;
+
+/**********************************************************************/
+// Global variables
+std::unordered_map<std::string, mat::matrix<matrix_t>> g_matrices;
+
+/**********************************************************************/
+// Forward declarations
+
+void
+printMatrix(tokenlist_t tokens);
+
+void
+transpose(tokenlist_t tokens);
+
+void
+inverse(tokenlist_t tokens);
+
+void
+rowEchelon(tokenlist_t tokens);
+
+void
+reducedRowEchelon(tokenlist_t tokens);
+
+void
+swapRows(tokenlist_t tokens);
+
+void
+addRows(tokenlist_t tokens);
+
+void
+multiplyRow(tokenlist_t tokens);
+
+void
+equalExpression(tokenlist_t tokens);
+
+/**********************************************************************/
+
+int
+main()
+{
+  g_matrices = std::unordered_map<std::string, mat::matrix<matrix_t>>();
+
+  std::string line;
+  while (std::getline(std::cin, line))
+  {
+    std::stringstream tokenize(line);
+    std::string token;
+    tokenlist_t tokens;
+
+    while (std::getline(tokenize, token, ' '))
+      tokens.push_back(token);
+
+    if (tokens[0] == "exit")
+      exit(0);
+    else if (tokens[0] == "reset")
+      g_matrices = std::unordered_map<std::string, mat::matrix<matrix_t>>();
+    else if (tokens[0] == "print")
+      printMatrix(tokens);
+    else if (tokens[0] == "transpose")
+      transpose(tokens);
+    else if (tokens[0] == "inverse")
+      inverse(tokens);
+    else if (tokens[0] == "row_echelon")
+      rowEchelon(tokens);
+    else if (tokens[0] == "reduced_row_echelon")
+      reducedRowEchelon(tokens);
+    else if (tokens[0] == "swap_rows")
+      swapRows(tokens);
+    else if (tokens[0] == "add_rows")
+      addRows(tokens);
+    else if (tokens[0] == "multiply_row")
+      multiplyRow(tokens);
+    else
+      equalExpression(tokens);
+      
+  }
+
+  return 0;
+}
+
+void
+printMatrix(tokenlist_t tokens)
+{
+  
+}
+
+void
+transpose(tokenlist_t tokens)
+{
+
+}
+
+void
+inverse(tokenlist_t tokens)
+{
+
+}
+
+void
+rowEchelon(tokenlist_t tokens)
+{
+
+}
+
+void
+reducedRowEchelon(tokenlist_t tokens)
+{
+
+}
+
+void
+swapRows(tokenlist_t tokens)
+{
+
+}
+
+void
+addRows(tokenlist_t tokens)
+{
+
+}
+
+void
+multiplyRow(tokenlist_t tokens)
+{
+
+}
+
+void
+equalExpression(tokenlist_t tokens)
+{
+
+}
 
 /*
  * Command list
@@ -45,10 +187,3 @@
  * reset
  * exit
  */
-
-int
-main()
-{
-  
-  return 0;
-}
