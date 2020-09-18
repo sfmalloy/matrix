217943e59bdaee95966f28283f4667e3a428e3ab
Sean Malloy <sfmalloy@millersville.edu>
2020-09-18 18:41:21 -0400

    Added functionality to equalsExpression() in main.cpp

--- main.cpp
+++ main.cpp
@@ -85,29 +85,33 @@ main()
     while (std::getline(tokenize, token, ' '))
       tokens.push_back(token);
 
-    if (tokens[0] == "exit")
-      exit(0);
-    else if (tokens[0] == "reset")
-      g_matrices = std::unordered_map<std::string, mat::matrix<elem_t>>();
-    else if (tokens[0] == "print")
-      printMatrix(tokens);
-    else if (tokens[0] == "transpose")
-      transpose(tokens);
-    else if (tokens[0] == "inverse")
-      inverse(tokens);
-    else if (tokens[0] == "row_echelon")
-      rowEchelon(tokens);
-    else if (tokens[0] == "reduced_row_echelon")
-      reducedRowEchelon(tokens);
-    else if (tokens[0] == "swap_rows")
-      swapRows(tokens);
-    else if (tokens[0] == "add_rows")
-      addRows(tokens);
-    else if (tokens[0] == "multiply_row")
-      multiplyRow(tokens);
-    else
-      equalExpression(tokens);
-      
+    if (tokens.size() > 0)
+    {
+      if (tokens[0] == "exit")
+        exit(0);
+      else if (tokens[0] == "reset")
+        g_matrices = std::unordered_map<std::string, mat::matrix<elem_t>>();
+      else if (tokens[0] == "print")
+        printMatrix(tokens);
+      else if (tokens[0] == "transpose")
+        transpose(tokens);
+      else if (tokens[0] == "inverse")
+        inverse(tokens);
+      else if (tokens[0] == "row_echelon")
+        rowEchelon(tokens);
+      else if (tokens[0] == "reduced_row_echelon")
+        reducedRowEchelon(tokens);
+      else if (tokens[0] == "swap_rows")
+        swapRows(tokens);
+      else if (tokens[0] == "add_rows")
+        addRows(tokens);
+      else if (tokens[0] == "multiply_row")
+        multiplyRow(tokens);
+      else if (tokens.size() > 1 && tokens[1] == "=")
+        equalExpression(tokens);
+      else
+        std::cerr << "No such command.\n" << '\n';
+    }  
   }
 
   return 0;
@@ -196,15 +200,57 @@ equalExpression(tokenlist_t tokens)
       g_matrices[name] = getRandom<elem_t>(rows, cols, lowerBound, upperBound);
 
   }
-  else if (keyword == "custom")
+  else if (keyword[0] == '[')
   {
+    // Concatenate list together into one string to tokenize list easier
+    std::string listStr;
+    for (size_t i = 0; i < tokens.size(); ++i)
+      if (tokens[i].back() != '=' && i > 0)
+        listStr += tokens[i];
+
+    // Count number of '[' and ']', if not equal then break
+    // Also count number of commas to figure out number of columns for later
+    int openCount = 0;
+    int closeCount = 0;
+    int commaCount = 0;
+    std::string numStr;
+    for (const char& c : listStr)
+    {
+      if (c == '[')
+        openCount += 1;
+      else if (c == ']')
+        closeCount += 1;
+      else if (c == ',' && closeCount == 0)
+        commaCount += 1;
+
+      if (c != '[' && c != ']')
+        numStr.push_back(c);
+    }
+
+    if (openCount != closeCount)
+    {
+      std::cerr << "Missing " << (openCount < closeCount ? '[' : ']') << ".\n";
+      return;
+    }
+
+    // By doing the above counting loop we now know the number of rows and columns
+    size_t numRows = openCount - 1;
+    size_t numCols = commaCount + 1;
     
+    std::stringstream tokenize(numStr);
+    std::string num;
+    mat::matrix<elem_t> A(numRows, numCols);
+    auto it = A.begin();
+
+    std::cout << numRows << ' ' << numCols << '\n';
+
+    // TODO: don't use stof and check the type to see if using double or float
+    while (std::getline(tokenize, num, ','))
+      *(it++) = std::stof(num);
+    g_matrices[name] = A;
   }
-  else
-  {
-    if (g_matrices.find(keyword) != g_matrices.end())
-      g_matrices[name] = g_matrices[keyword];
-  }
+  else if (g_matrices.find(keyword) != g_matrices.end())
+    g_matrices[name] = g_matrices[keyword];
 }
 
 template <typename T>
